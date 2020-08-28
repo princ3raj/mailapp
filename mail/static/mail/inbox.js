7795209f2f705419c1e0976f5c38278e74298175
princ3raj <princ3raj1999@gmail.com>
2020-08-29 03:45:01 +0530

    checked all mailboxes , all working

---
+++ mail/static/mail/inbox.js
@@ -0,0 +1,72 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  // Use buttons to toggle between views
+  document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
+  document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
+  document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
+  document.querySelector('#compose').addEventListener('click', compose_email);
+
+  // By default, load the inbox
+  load_mailbox('inbox');
+});
+
+function compose_email() {
+
+  // Show compose view and hide other views
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'block';
+
+  // Clear out composition fields
+  document.querySelector('#compose-recipients').value = '';
+  document.querySelector('#compose-subject').value = '';
+  document.querySelector('#compose-body').value = '';
+
+
+  document.querySelector('#submit').addEventListener('click',()=>{
+
+    var recipentmessage = document.querySelector('#compose-recipients').value;
+    var subjectmessage= document.querySelector('#compose-subject').value;
+    var bodymessage = document.querySelector('#compose-body').value;
+
+    
+
+
+    fetch('/emails', {
+     method: 'POST',
+     body: JSON.stringify({
+         recipients:recipentmessage,
+         subject:subjectmessage,
+         body:bodymessage
+     })
+   })
+   .then(response => response.json())
+   .then(result => {
+       // Print result
+       console.log(result);
+   });
+
+
+ 
+
+
+
+ })
+}
+
+function load_mailbox(mailbox) {
+  
+  // Show the mailbox and hide other views
+  document.querySelector('#emails-view').style.display = 'block';
+  document.querySelector('#compose-view').style.display = 'none';
+
+  // Show the mailbox name
+  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  fetch('/emails/archive')
+  .then(response => response.json())
+  .then(emails => {
+    // Print emails
+    console.log(emails);
+
+    // ... do something else with emails ...
+}); 
+}
